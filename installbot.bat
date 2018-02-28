42f0827bd9c5c41c444f5bfc5564de5284ed0bc0
ChisdealHD <Chisdeal2014@hotmail.com>
2018-02-28 23:03:39 +0000

    Installation index
    index

---
+++ installbot.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+echo Installing Modules...
+npm install clear chalk request steam-tradeoffer-manager steamcommunity steam-totp steam-user --save
+echo Please go to Bots folder and Install Modules there too.
---
+++ installbot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# If you have issues running this try this code
+# sudo chmod 777 runForever.sh
+echo Installing Modules...
+npm install clear chalk request steam-tradeoffer-manager steamcommunity steam-totp steam-user --save
+echo Please go to Bots folder and Install Modules there too.
