153446934c70a370f2b715e7c47ad69bef5805d9
ChisdealHD <Chisdeal2014@hotmail.com>
2018-02-28 23:06:26 +0000

    installation discord index
    Meh another 1 :/

---
+++ discord/installbot.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+echo Installing Modules...
+npm install clear chalk request get-json util request node-schedule markdown bluebird discord.js --save
+echo Please go to Bots folder and Install Modules there too.
---
+++ discord/installbot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# If you have issues running this try this code
+# sudo chmod 777 runForever.sh
+echo Installing Modules...
+npm install clear chalk request get-json util request node-schedule markdown bluebird discord.js --save
+echo Please go to Bots folder and Install Modules there too.
